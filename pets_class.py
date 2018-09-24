1b406998323e386902d84626db1db63033db87fa
calvinpete <tinkacalvin@gmail.com>
2018-09-24 12:49:41 +0300

    add eat method and pets_meal method

--- pets_class.py
+++ pets_class.py
@@ -18,7 +18,7 @@ class Pets:
         This outputs the number of instances of the Dog class stored in the data structure
         :return:
         """
-        print("I have {} dogs".format(len(self.records)))
+        print("I have {} dogs\n".format(len(self.records)))
 
     def pet_profile(self):
         """
@@ -27,7 +27,7 @@ class Pets:
         """
         for i in self.records:
             #  The __getattribute__ provides access to the instance attributes
-            print("{0} is {1}".format(i.__getattribute__("name"), i.__getattribute__("age")))
+            print("{0} is {1}\n".format(i.__getattribute__("name"), i.__getattribute__("age")))
 
     def animal_class(self):
         """
@@ -37,20 +37,39 @@ class Pets:
         for i in self.records:
             #  The __getattribute__ provides access to the instance attributes
             if i.__getattribute__("kind") != "mammal":
-                print("one of them is not a mammal.")
+                print("one of them is not a mammal.\n")
                 return self.records
         else:
-            print("And they're all mammals, of course.")
+            print("And they're all mammals, of course.\n")
+
+    def pets_meal(self):
+        """
+        This checks if an instance of the dog class is hungry then the instance is fed
+        :return:
+        """
+        for i in self.records:
+            if i.__getattribute__("is_hungry"):
+                i.eat()
+        else:
+            print("My dogs are hungry\n")
 
 
 class Dog:
     """
     This class holds the profile information of a dog
     """
-    def __init__(self, name, age, kind):
+    def __init__(self, name, age, kind, is_hungry=True):
         self.name = name
         self.age = age
         self.kind = kind
+        self.is_hungry = is_hungry
+
+    def eat(self):
+        """
+        This changes the value of is_hungry to False
+        :return:
+        """
+        self.is_hungry = False
 
 
 if __name__ == "__main__":
@@ -64,3 +83,4 @@ if __name__ == "__main__":
     my_pets.pets_owned()
     my_pets.pet_profile()
     my_pets.animal_class()
+    my_pets.pets_meal()
