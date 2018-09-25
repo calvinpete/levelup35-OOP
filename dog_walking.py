6e9b196299f88df5f66d9543bc35dd09ff0819b6
calvinpete <tinkacalvin@gmail.com>
2018-09-25 14:26:12 +0300

    employ an insatnce variable in the Pets class

--- dog_walking.py
+++ dog_walking.py
@@ -1,51 +1,55 @@
+class Dog:
+    """
+    This class holds the profile information of a dog
+    """
+    animal_class = "mammal"
+
+    def __init__(self, name, age, is_hungry=True):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+
+    def eat(self):
+        """
+        This changes the value of is_hungry to False
+        :return:
+        """
+        self.is_hungry = False
+
+    def walk(self):
+        """
+        This outputs the behaviour of the instance of the Dog class
+        :return self.name is walking!:
+        """
+        return "{} is walking!\n".format(self.name)
+
+
 class Pets:
     """
     This class holds instances of the Dog class
     """
-    def __init__(self):
-        self.records = []  # allows storage of data outside the class
+    records = []  # allows storage of data outside the class
 
-    def add_pet(self, pet):
+    def __init__(self, pet_dogs):
+        self.pet_dogs = pet_dogs  # instance variable
+
+    def pets_owned(self):
         """
-        Stores an instance of the Dog class in a data structure
-        :param pet:
-        :return:
+        This outputs the number of instances of the Dog class stored in the data structure
+        :return I have len(self.pet_dogs) dogs:
         """
-        self.records.append(pet)
+        return "I have {} dogs\n".format(len(self.pet_dogs))
 
     def walk(self):
         """
         This calls the walk method for each instance of the Dog class
         :return:
         """
-        for i in self.records:
-            i.walk()
-
-
-class Dog:
-    """
-    This class holds the profile information of a dog
-    """
-    def __init__(self, name, age, kind, is_hungry=True):
-        self.name = name
-        self.age = age
-        self.kind = kind
-        self.is_hungry = is_hungry
-
-    def walk(self):
-        """
-        This outputs the behaviour of the instance of the Dog class
-        :return:
-        """
-        print("{} is walking.\n".format(self.name))
+        for dog in self.pet_dogs:
+            print(dog.walk())
 
 
 if __name__ == "__main__":
-    tom = Dog("Tom", 6, "mammal")
-    fletcher = Dog("Fletcher", 7, "mammal")
-    larry = Dog("Larry", 9, "mammal")
-    my_pets = Pets()
-    my_pets.add_pet(tom)
-    my_pets.add_pet(fletcher)
-    my_pets.add_pet(larry)
+    records = [Dog("tom", 6), Dog("Fletcher", 7), Dog("Larry", 9)]
+    my_pets = Pets(records)
     my_pets.walk()
--- pets_class.py
+++ pets_class.py
@@ -1,67 +1,12 @@
-class Pets:
-    """
-    This class holds instances of the Dog class
-    """
-    def __init__(self):
-        self.records = []  # allows storage of data outside the class
-
-    def add_pet(self, pet):
-        """
-        Stores an instance of the Dog class in a data structure
-        :param pet:
-        :return:
-        """
-        self.records.append(pet)
-
-    def pets_owned(self):
-        """
-        This outputs the number of instances of the Dog class stored in the data structure
-        :return:
-        """
-        print("I have {} dogs\n".format(len(self.records)))
-
-    def pet_profile(self):
-        """
-        This outputs the information of each instance of the Dog class
-        :return:
-        """
-        for i in self.records:
-            #  The __getattribute__ provides access to the instance attributes
-            print("{0} is {1}\n".format(i.__getattribute__("name"), i.__getattribute__("age")))
-
-    def animal_class(self):
-        """
-        This checks for a specific attribute of each instance of the Dog class in the data structure
-        :return:
-        """
-        for i in self.records:
-            #  The __getattribute__ provides access to the instance attributes
-            if i.__getattribute__("kind") != "mammal":
-                print("one of them is not a mammal.\n")
-                return self.records
-        else:
-            print("And they're all mammals, of course.\n")
-
-    def pets_meal(self):
-        """
-        This checks if an instance of the dog class is hungry then the instance is fed
-        :return:
-        """
-        for i in self.records:
-            if i.__getattribute__("is_hungry"):
-                i.eat()
-        else:
-            print("My dogs are hungry\n")
-
-
 class Dog:
     """
     This class holds the profile information of a dog
     """
-    def __init__(self, name, age, kind, is_hungry=True):
+    animal_class = "mammal"
+
+    def __init__(self, name, age, is_hungry=True):
         self.name = name
         self.age = age
-        self.kind = kind
         self.is_hungry = is_hungry
 
     def eat(self):
@@ -72,15 +17,39 @@ class Dog:
         self.is_hungry = False
 
 
+class Pets:
+    """
+    This class holds instances of the Dog class
+    """
+    records = []  # allows storage of data outside the class
+
+    def __init__(self, pet_dogs):
+        self.pet_dogs = pet_dogs  # instance variable
+
+    def pets_owned(self):
+        """
+        This outputs the number of instances of the Dog class stored in the data structure
+        :return:
+        """
+        return "I have {} dogs\n".format(len(self.pet_dogs))
+
+
 if __name__ == "__main__":
-    tom = Dog("Tom", 6, "mammal")
-    fletcher = Dog("Fletcher", 7, "mammal")
-    larry = Dog("Larry", 9, "mammal")
-    my_pets = Pets()
-    my_pets.add_pet(tom)
-    my_pets.add_pet(fletcher)
-    my_pets.add_pet(larry)
+    records = [Dog("tom", 6), Dog("Fletcher", 7), Dog("Larry", 9)]
+    my_pets = Pets(records)
     my_pets.pets_owned()
-    my_pets.pet_profile()
-    my_pets.animal_class()
-    my_pets.pets_meal()
+    for dog in my_pets.pet_dogs:
+        print("{0} is {1}\n".format(dog.name, dog.age))
+    else:
+        print("And they're all {}s, of course.\n".format(dog.animal_class))
+
+    for dog in my_pets.pet_dogs:
+        if dog.is_hungry:
+            dog.eat()
+
+    if dog.is_hungry:
+        print("My dogs are hungry")
+    else:
+        print("My dogs are not hungry")
+
+
